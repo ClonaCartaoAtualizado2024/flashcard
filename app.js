61b299b9465d2d09d554009f9ad2e3ee21bc2363
ClonaCartaoAtualizado2024 <163008911+ClonaCartaoAtualizado2024@users.noreply.github.com>
2024-10-16 12:18:51 -0300

    .

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ function criaCartao (categoria, pergunta, resposta) {
 cartao.innerHTML = `
   <div class="cartao__conteudo">
    <h3>${categoria}</h3>
-   <div class="cartao__conteudo__perguta">
-   <p>${pergunta}</p>
+   <div class="cartao__conteudo__pergunta">
+    <p>${pergunta}</p>
    </div>
-   <div class="cartao__pergunta">
-   <p>${resposta}</p>
+   <div class="cartao__conteudo__resposta">
+    <p>${resposta}</p>
    </div>
   </div> 
   `;
 let respostaEstaVisivel = false;
 
 function viraCartao() {
-    respostaEstaVisivel = !respostaEstaVisivel 
-    cartao.classList.toggle('active', respostaEstaVisivel)
-}
-  cartao.addEventListener('click', viraCartao)
+  respostaEstaVisivel = !respostaEstaVisivel;
+  cartao.classList.toggle('active', respostaEstaVisivel);
+
+  cartao.addEventListener('click', viraCartao);
   
-  container.appendChild(cartao)
-  }
+  container.appendChild(cartao);
+}
